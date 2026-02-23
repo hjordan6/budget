25fb7d9eac5e465e9922c946ec72baf29d5b3245
Jordan Hall <jordanhallnc@gmail.com>
2026-02-23 08:15:14 -0700

    fixed it

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Building Flutter web project..."
-flutter build web
+flutter build web --release
 echo "Deploying to Firebase Hosting..."
 firebase deploy --only hosting
 echo "Deployment complete!"
