c3255f7e11683e758662d911d8565e35dc7c5ce3
Jordan Hall <jordanhallnc@gmail.com>
2026-02-25 01:04:05 -0700

    add action

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-echo "Building Flutter web project..."
-flutter build web --release
+echo "Building Flutter web project in DEV..."
+flutter build web --release --dart-define=FIREBASE_ENV=dev
 echo "Deploying to Firebase Hosting..."
 firebase deploy --only hosting
 echo "Deployment complete!"
